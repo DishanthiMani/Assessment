d49028577e1c2509cbae827e40e37184e85057b6
DishanthiMani <dishanthi@zirius.in>
2022-03-30 19:33:22 -0700

    Implemented-Detailspage-css

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,39 @@
+var date = new Date().toISOString().split("T")[0];
+
+function locationFrom() {
+    var travelLocation = ["Ariyalur", "Chengalpattu", "Chennai", "Coimbatore", "Cuddalore", "Dharmapuri", "Dindigul", "Erode", "Kallakurichi", "Kanchipuram", "Kanyakumari", "Karur", "Krishnagiri", "Madurai", "Nagapattinam", "Namakkal", "Nilgiris", "Perambalur", "Pudukkottai", "Ramanathapuram", "Ranipet", "Salem", "Sivaganga", "Tenkasi", "Thanjavur", "Theni", "Tuticorin", "Tiruchirappalli", "Tirunelveli", "Tirupathur", "Tiruppur", "Tiruvallur", "Tiruvannamalai", "Tiruvarur", "Vellore", "Viluppuram", "Virudhunagar"]
+    travelLocation.forEach(element => {
+        var options = document.createElement('option');
+        var text = document.createTextNode(element)
+        options.appendChild(text);
+        document.getElementById('from').appendChild(options);
+    });
+}
+
+function locationTo() {
+    var travelLocation = ["Ariyalur", "Chengalpattu", "Chennai", "Coimbatore", "Cuddalore", "Dharmapuri", "Dindigul", "Erode", "Kallakurichi", "Kanchipuram", "Kanyakumari", "Karur", "Krishnagiri", "Madurai", "Nagapattinam", "Namakkal", "Nilgiris", "Perambalur", "Pudukkottai", "Ramanathapuram", "Ranipet", "Salem", "Sivaganga", "Tenkasi", "Thanjavur", "Theni", "Tuticorin", "Tiruchirappalli", "Tirunelveli", "Tirupathur", "Tiruppur", "Tiruvallur", "Tiruvannamalai", "Tiruvarur", "Vellore", "Viluppuram", "Virudhunagar"]
+    travelLocation.forEach(element => {
+        var options = document.createElement('option');
+        var text = document.createTextNode(element)
+        options.appendChild(text);
+        document.getElementById('to').appendChild(options);
+    });
+
+}
+
+function locationValidation() {
+    var startingLocation = document.getElementById("from").value;
+    var locationSpanId = document.getElementById("invalidlocation");
+    var destination = document.getElementById("to").value;
+    if (startingLocation == destination) {
+        locationSpanId.classList.remove("location-error");
+        locationSpanId.style.fontSize = "smaller";
+        locationSpanId.style.color = "red";
+    } else {
+        locationSpanId.classList.add("location-error");
+    }
+}
+
 function myFunction() {
     var password = document.getElementById("pass");
     if (password.type === "password") {
@@ -26,6 +62,7 @@ function validate() {
             Confirm: document.getElementById("confirmpass").value,
             Gender: document.getElementById("male").checked ? "male" : "female"
         }
+        console.log(JSON.stringify(person))
         window.localStorage.setItem('person', JSON.stringify(person));
         var url = new URL("http://127.0.0.1:5500/html/index.html");
         window.location.href = url;
@@ -39,7 +76,7 @@ function nametest() {
     var nameInputBox = document.getElementById("name");
     if (nameRegex.test(nameValue)) {
         nameSpanId.classList.add("errormessage");
-        nameInputBox.style.borderColor = "gray";
+        nameInputBox.style.borderColor = "grey";
     } else {
         nameSpanId.classList.remove("errormessage");
         nameSpanId.style.color = "red";
@@ -54,7 +91,7 @@ function emailtest() {
     var emailInputBox = document.getElementById("email");
     if (emailRegex.test(emailValue)) {
         emailSpanId.classList.add("errormessage");
-        emailInputBox.style.borderColor = "gray";
+        emailInputBox.style.borderColor = "grey";
     } else {
         emailSpanId.classList.remove("errormessage");
         emailSpanId.style.color = "red";
@@ -69,7 +106,7 @@ function contactvalidate() {
     var mobileInputBox = document.getElementById("mobile");
     if (mobileRegex.test(mobileValue)) {
         mobileSpanId.classList.add("errormessage");
-        mobileInputBox.style.borderColor = "gray";
+        mobileInputBox.style.borderColor = "grey";
     } else {
         mobileSpanId.classList.remove("errormessage");
         mobileSpanId.style.color = "red";
@@ -84,7 +121,7 @@ function passwordvalidate() {
     var passwordInputBox = document.getElementById("pword");
     if (passwordRegex.test(passwordValue)) {
         passwordSpanId.classList.add("errormessage");
-        passwordInputBox.style.borderColor = "gray";
+        passwordInputBox.style.borderColor = "grey";
     } else {
         passwordSpanId.classList.remove("errormessage");
         passwordSpanId.style.color = "red";
@@ -99,7 +136,7 @@ function confirmpassword() {
     var confirmInputBox = document.getElementById("confirmpass");
     if (givenpassword == confirmpass) {
         confirmSpanId.classList.add("errormessage");
-        confirmInputBox.style.borderColor = "gray";
+        confirmInputBox.style.borderColor = "grey";
     } else {
         confirmSpanId.classList.remove("errormessage");
         confirmSpanId.style.color = "red";
@@ -124,12 +161,50 @@ function loginpage() {
     window.location.href = loginpage;
 }
 
-function amouncalculation() {
+function startingDate() {
+    var todayDate = new Date().toISOString().split("T")[0];
+    if (document.getElementById("leavingdate") != null) {
+        document.getElementById("leavingdate").setAttribute("min", todayDate);
+    }
+}
+
+function journeyDate() {
+    var journeydate = document.getElementById("leavingdate").value;
+    var date1 = new Date(journeydate);
+    console.log(date1)
+    console.log(typeof date1);
+    var date = date1.getDate() + 2;
+    var year = date1.getFullYear();
+    var month = date1.getMonth() + 1;
+    var minimumDate = year + "-" + month + "-" + date;
+    document.getElementById("returndate").setAttribute("min", minimumDate);
+    console.log(minimumDate);
 
 }
 
+function endingDate() {
+    var x = document.getElementById("leavingdate").value;
+    console.log(x);
+    console.log(typeof x);
+    var date1 = new Date(x);
+    console.log(date1)
+    console.log(typeof date1);
+    var date = date1.getDate() + 1;
+    var year = date1.getFullYear();
+    var month = date1.getMonth() + 1;
+    var minimumDate = year + "-" + month + "-" + date;
+    document.getElementById("returndate").setAttribute("min", minimumDate);
+    console.log(minimumDate);
+}
+
+function birthDate() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear() - 10;
+    var birthday = currentYear + "-12-31";
+    document.getElementById("dob").setAttribute("max", birthday);
+}
+
 function increment() {
-    //var amount = document.getElementById("donation").value;
     document.getElementById("donation").stepUp();
 }
 
