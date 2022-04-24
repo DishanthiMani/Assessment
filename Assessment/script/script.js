b4d4cb26ddad056f3d6c1f5db7d5d3076bca04d4
DishanthiMani <dishanthi@zirius.in>
2022-04-23 20:44:01 -0700

    Fix - Error Handling, Validation

--- Assessment/script/script.js
+++ Assessment/script/script.js
@@ -1,11 +1,12 @@
-document.cookie = ("Bearer=eyJraWQiOiJhZm5VVTd6STJzdk1ISEcydkl3eE44enlxU0NXck1NNSttUDUxYTZcL0Uydz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJjNzRjYjg0OS0xNDQ5LTQ0YWUtYmU3YS0wNGU0OTRhNDczYmIiLCJhdWQiOiI3dDgwNzYzN3Q5bmdwYmI1ZHZrOWIwbXV0NSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJldmVudF9pZCI6IjUzZjQyNjRmLWM1ZTQtNDdmMi1iNzBmLWZhMTQ2Mzk4ODVkYyIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjQ5ODI0MTIyLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtbm9ydGgtMS5hbWF6b25hd3MuY29tXC9ldS1ub3J0aC0xXzZzMGFMblZFRSIsImNvZ25pdG86dXNlcm5hbWUiOiJjNzRjYjg0OS0xNDQ5LTQ0YWUtYmU3YS0wNGU0OTRhNDczYmIiLCJleHAiOjE2NDk4Mjc3MjIsImlhdCI6MTY0OTgyNDEyMiwiZW1haWwiOiJnb3V0aGFtQHppcml1cy5pbiJ9.Uc1A5IKi2x3ArKCG-R9G9CtRU67HTeZd1lPXcwWoQcnWkPDUSLCdrThTZvlppZu0iGVMTnR-b2n1FgwtDTiZZHz3lHWuyr3jrXWBt4K5zQkrLC86Vl6HxZ0UqZKz4uzkWEAKfGPyzJe6QkkGgqNEJD_Ki_KAWT7qyRTYlIVpg5Yg9u-pAZ-hLA4yOM8dz9_jYnosN4D_la7sf_GUfpDDT_lZA1pE7qmhPp7U5HLML7sC8F6gFT2JSwJdRjMc8OU7t-7yY97LH5tIB93OM2hjpUzX5ldBOJPVLuBF6m1c1VJvfEyrhdi1Mmsz11hYEAFOv3sPn7EnwzmKz-ur3dP3kw");
+document.cookie = ("Bearer=eyJraWQiOiJhZm5VVTd6STJzdk1ISEcydkl3eE44enlxU0NXck1NNSttUDUxYTZcL0Uydz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJjNzRjYjg0OS0xNDQ5LTQ0YWUtYmU3YS0wNGU0OTRhNDczYmIiLCJhdWQiOiI3dDgwNzYzN3Q5bmdwYmI1ZHZrOWIwbXV0NSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJldmVudF9pZCI6IjI2NGViZjA0LWE1YTQtNDc2Ni1iMTkxLWM4NTUyNTk3MGJkZCIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUwNjg1MDAwLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtbm9ydGgtMS5hbWF6b25hd3MuY29tXC9ldS1ub3J0aC0xXzZzMGFMblZFRSIsImNvZ25pdG86dXNlcm5hbWUiOiJjNzRjYjg0OS0xNDQ5LTQ0YWUtYmU3YS0wNGU0OTRhNDczYmIiLCJleHAiOjE2NTA2ODg2MDAsImlhdCI6MTY1MDY4NTAwMCwiZW1haWwiOiJnb3V0aGFtQHppcml1cy5pbiJ9.F8FVjdb7SbLKf_x3Y8qsJH5Vri_wJYRF4lgOS0jVNlFNnc0Pn4PILQrUweSNmUkof21kn3HceNT7k8sXYX2CmaS89k0_2ukSMkjQr7U8FLnV6ClastQUT--PFGZlfzJnX1PeYTxCGr-dOOpsrGecf2zmUDLyDMf9k7BGrD3B1Fh06sKR3fgNlnVS9yKdyyHjJZv44Y40MB-sVq86BLkGkjzPuYq7mlpPONVXZt9OA4iwyTyClHvmEXpH5dYBsIqDeQGzvlL7YPFfnsnpaAt99xsQz0v5SbYEXWV4aDwd-rKkl5qkp0QVMmbvnOw3YG6zUPrseNJEW8DOnEcdVaUa6Q");
 var id;
+var indexPage = "http://localhost/expense/Assessment/html/index.html";
 
 function load() {
  getEmployeeName();
 }
 
- function getEmployeeName(){
+function getEmployeeName() {
 
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET","http://localhost/ec/employees",true);
@@ -14,7 +15,7 @@ function load() {
     xhttp.setRequestHeader("Authorization","Bearer "+ document.cookie.split("=")[1]);
     xhttp.setRequestHeader("identity",document.cookie.split("=")[1]);
     xhttp.send();
-    xhttp.onload = function(){
+    xhttp.onload = function() {
         if(this.status == 200) { 
             var employeelist = JSON.parse(this. responseText);
            employeelist.dropdownList.forEach(employee => {
@@ -29,7 +30,8 @@ function load() {
     }
     
 }
- function getPaymentType(){
+
+ function getPaymentType() {
 
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET","../paymenttype.json",true);
@@ -38,11 +40,11 @@ function load() {
     xhttp.setRequestHeader("Authorization","Bearer"+ document.cookie.split("=")[1]);
     xhttp.setRequestHeader("identity",document.cookie.split("=")[1]);
     xhttp.send();
-    xhttp.onreadystatechange = function(){
-        if(this.status == 200 && this.readyState == 4){
-        getCurrencyType();
+    xhttp.onreadystatechange = function() {
+        if(this.status == 200 && this.readyState == 4) {
+            getCurrencyType();
             var paymentlist = JSON.parse(this.responseText);
-            paymentlist.ledgerTemplates.forEach(paymenttype =>{
+            paymentlist.ledgerTemplates.forEach(paymenttype => {
                 var option = document.createElement("option");
                 option.value = paymenttype.id;
                 var text = document.createTextNode(paymenttype.name);
@@ -53,7 +55,9 @@ function load() {
     }
 
 }
-  function getCurrencyType(){
+
+function getCurrencyType() {
+
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET","http://localhost/ec/currencies", true);
     xhttp.setRequestHeader("companyId","14");
@@ -61,22 +65,24 @@ function load() {
     xhttp.setRequestHeader("Authorization","Bearer"+document.cookie.split("=")[1]);
     xhttp.setRequestHeader("identity",document.cookie.split("=")[1]);
     xhttp.send();
-    xhttp.onreadystatechange = function(){
-        if(this.status == 200 && this.readyState == 4){
-            paymentMethod();
-            var currencies = JSON.parse(this.responseText);
-            currencies.currencyList.forEach(currency =>{
-            var option = document.createElement("option");
-            option.value = currency.currencyCode;
-            var text = document.createTextNode(currency.currencyName);
-            option.appendChild(text);
-            document.getElementById("currencyType").appendChild(option);
-        });
+    xhttp.onreadystatechange = function() {
+        if(this.status == 200 && this.readyState == 4) {
+                paymentMethod();
+                var currencies = JSON.parse(this.responseText);
+                currencies.currencyList.forEach(currency =>{
+                var option = document.createElement("option");
+                option.value = currency.currencyCode;
+                var text = document.createTextNode(currency.currencyName);
+                option.appendChild(text);
+                document.getElementById("currencyType").appendChild(option);
+            });
         }
     }
 
 }
- function paymentMethod(){
+
+function paymentMethod() {
+
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET","http://localhost/ec/paymentMethod",true);
     xhttp.setRequestHeader("companyId","14");
@@ -84,9 +90,9 @@ function load() {
     xhttp.setRequestHeader("Authorization","Bearer" +document.cookie.split("=")[1]);
     xhttp.setRequestHeader("identity",document.cookie.split("=")[1]);
     xhttp.send();
-    xhttp.onreadystatechange = function(){
-        if(this.status == 200 && this.readyState == 4){
-        displayEditpage();
+    xhttp.onreadystatechange = function() {
+        if(this.status == 200 && this.readyState == 4) {
+            displayEditpage();
             var paymentmethod = JSON.parse(this.responseText);
             paymentmethod.dropdownList.forEach(method =>{
                 var option = document.createElement("option");
@@ -98,52 +104,55 @@ function load() {
         }
     }
 }
-function dateRestriction(){
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; 
-var yyyy = today.getFullYear();
-if(dd < 11){
-    if (dd < 10) {
-           dd = '0' + dd;
+
+function dateRestriction() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; 
+    var yyyy = today.getFullYear();
+    if(dd < 11) {
+        if (dd < 10) {
+            dd = '0' + dd;
         }
         if (mm < 10) {
-               mm = '0' + mm;
-            }   
-    today = yyyy + '-' + mm + '-' + dd;
-    document.getElementById("paymentDate").setAttribute("max", today );
-}else{
-    if (dd < 10) {
-        dd = '0' + dd;
-     }
-     if (mm < 10) {
-            mm = '0' + mm;
-         }
-    today = yyyy + '-' + mm + '-' + 10;
-    console.log(today);
-    document.getElementById("paymentDate").setAttribute("max", today);
-    console.log(document.getElementById("paymentDate"));   
+                mm = '0' + mm;
+        }   
+        today = yyyy + '-' + mm + '-' + dd;
+        document.getElementById("paymentDate").setAttribute("max", today );
+    }
+    else {
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+        if (mm < 10) {
+                mm = '0' + mm;
+        }
+        today = yyyy + '-' + mm + '-' + 10;
+        document.getElementById("paymentDate").setAttribute("max", today);   
+    }
+    // if (dd < 10) {
+    //    dd = '0' + dd;
+    // }
+    // if (mm < 10) {
+    //    mm = '0' + mm;
+    // }    
+    // today = yyyy + '-' + mm + '-' + dd;
+    // document.getElementById("paymentDate").setAttribute("max", today);
 }
-// if (dd < 10) {
-//    dd = '0' + dd;
-// }
-// if (mm < 10) {
-//    mm = '0' + mm;
-// }    
-// today = yyyy + '-' + mm + '-' + dd;
-// document.getElementById("paymentDate").setAttribute("max", today);
 
-}
-function payoutCheckbox(){
-    if(document.getElementById("payoutCheckbox").checked){
+function payoutCheckbox() {
+
+    if(document.getElementById("payoutCheckbox").checked) {
         payoutCheckbox.value = "Yes";
         return true;
-    }else{
+    }else {
         payoutCheckbox.value = "No";
         return false;
     }
 }
-function postJSONData(){
+
+function postJSONData() {
+
     var expenseData = {
         attachments : [],
         amount : parseFloat(document.getElementById("totalAmount").value),
@@ -182,11 +191,15 @@ function postJSONData(){
     }
     
 }
-function getExpenseData(){
-                   var display = new URL("http://localhost/expense/html/expensepage.html");
+
+function getExpenseData() {
+
+                   var display = new URL("http://localhost/expense/Assessment/html/expensepage.html");
                    window.location.href = display;
 }
-function loadExpenseData(){
+
+function loadExpenseData() {
+
     var stringData = JSON.stringify({});
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST","http://localhost/ec/expenses/stage/Inbox?count=10&offset=0",true);
@@ -267,7 +280,7 @@ function loadExpenseData(){
                     var deleteButton = document.createElement("button");
                     deleteButton.setAttribute("id","deleteButton"+i);
                     deleteButton.setAttribute("class","delete-button");
-                    deleteButton.setAttribute("onclick","deleteData(this)");
+                    deleteButton.setAttribute("onclick","confirmAction()");
                     deleteButton.innerText = "Delete"
                     childDiv.append(deleteButton);
                     document.getElementsByClassName("expense-cards")[0].appendChild(formDiv);
@@ -277,8 +290,18 @@ function loadExpenseData(){
     }
    
 }
-function deleteData(element){
-    var deleteData = element.parentElement.parentElement.getElementsByClassName("id-input")[0].value;
+
+function confirmAction() {
+
+    var confirmation = confirm("Are you sure to delete?")
+    if(confirmation){
+        deleteData();
+    }
+}
+
+function deleteData() {
+
+    var deleteData = document.getElementsByClassName("id-input")[0].value;
     var deleteReq = new XMLHttpRequest();
     deleteReq.open("DELETE","http://localhost/ec/expense/"+deleteData,true);
     deleteReq.setRequestHeader("Accept","application/JSON");
@@ -289,36 +312,42 @@ function deleteData(element){
     deleteReq.onreadystatechange = function(){
         if(this.readyState == 4){
             if(this.status == 200){
-                alert("Data deleted successfully");
-            document.getElementsByClassName("expense-cards")[0].innerHTML = "";
-            loadExpenseData();
-        }
+                var response = JSON.parse(this.responseText);
+                alert(response.message);
+                document.getElementsByClassName("expense-cards")[0].innerHTML = "";
+                loadExpenseData();
+            }
         }
     }
 }
-function dateFormat(dateOfPayment){
+
+function dateFormat(dateOfPayment) {
+
     var dateObject = new Date(dateOfPayment);
     var dd = dateObject.getDate();
     var mm = dateObject.getMonth()+1;
     var year = dateObject.getFullYear();
     if (dd < 10) {
         dd = '0' + dd;
-     }
-     if (mm < 10) {
+    }
+    if (mm < 10) {
             mm = '0' + mm;
-         }
+    }
     return mm + "-" + dd + "-" + year;
     
 }
 
-function editData(req){
+function editData(req) {
+
     var editdata = req.parentElement.parentElement.getElementsByClassName("id-input")[0].value;
-    var url = new URL(`http://localhost/expense/html/index.html?id=${editdata}`);
+    var url = new URL(indexPage+`?id=${editdata}`);
     window.location.href = url;
 }
- function displayEditpage(){
+
+function displayEditpage() {
+
     var param = new URLSearchParams(window.location.search);
-    if(param.get("id") != null ){
+    if(param.get("id") != null ) {
         id = param.get("id");
         document.getElementById("saveData").value = "Update";
         var expenseid = param.get("id");
@@ -328,16 +357,16 @@ function editData(req){
         xhttp.setRequestHeader("Authorization","Bearer "+document.cookie.split("=")[1]);
         xhttp.setRequestHeader("identity", "Bearer "+document.cookie.split("=")[1]);
         xhttp.send();
-        xhttp.onreadystatechange = function(){
-            if(this.readyState == 4){
-                if(this.status == 200){
+        xhttp.onreadystatechange = function() {
+            if(this.readyState == 4) {
+                if(this.status == 200) {
                     var response = JSON.parse(this.responseText);
                     document.getElementById("selectName").value = response.expense.employee.userId;
                     document.getElementById("expenseType").value = response.expense.name;
                     document.getElementById("paymenttypeName").value = parseInt( response.expense.paymentType.id);
                     document.getElementById("paymentMethod").value = response.expense.paymentMethod.id;
                     document.getElementById("paymentDate").value = response.expense.invoiceDate.split("T")[0];
-                    document.getElementById("payoutCheckbox").value = response.expense.payoutWithSalary;
+                    document.getElementById("payoutCheckbox").checked = response.expense.payoutWithSalary;
                     document.getElementById("expenseNotes").value = response.expense.notes;
                     document.getElementById("totalAmount").value = response.expense.amount;
                     fixedValue();
@@ -347,7 +376,8 @@ function editData(req){
         }
     }
 }
-function putMethod(){
+
+function putMethod() {
 
     var putReq = new XMLHttpRequest();
     putReq.open("PUT","http://localhost/ec/expense?id="+id,"true");
@@ -376,68 +406,109 @@ function putMethod(){
         lineItems : [],
         dimensions :[]   
     }
-    putReq.onreadystatechange = function(){
-        if(this.readyState == 4){
-            if(this.status == 200){
-                alert("Data edited succesfully");
+    putReq.onreadystatechange = function() {
+        if(this.readyState == 4) {
+            if(this.status == 200) {
+                var response = JSON.parse(this.responseText);
+                alert(response.message);
                 getExpenseData();
+            }else {
+                alert(response.message);
+                postJSONData();
             }
         }
     }
     var putstringData = JSON.stringify(putData);
     putReq.send(putstringData);   
 }
-function fixedValue(){
+
+function fixedValue() {
+
     var paidAmount = document.getElementById("totalAmount").value;
-    console.log(typeof(paidAmount));
     var amountPaid = parseFloat(paidAmount).toFixed(2);
     document.getElementById("totalAmount").value = amountPaid;
-    console.log(amountPaid);
 }
-function formValidation(){
-    if(document.forms[0].checkValidity()){
+
+function formValidation() {
+    // console.log("formValidation");
+    // var nameField = document.getElementById("selectName");
+    // var ExpenseNameField = document.getElementById("expenseType");
+    // var paymentTypeField = document.getElementById("paymenttypeName");
+    // var paymentMethodField = document.getElementById("paymentMethod");
+    // var paymentDateField = document.getElementById("paymentDate");
+    // var paymentAmountField = document.getElementById("totalAmount");
+    // var currencyTypeField = document.getElementById("currencyType");
+
+    // if(!nameField.checkValidity()) {
+    //     var parentForm = document.getElementById("selectName").value;
+    //     var nameSpan = document.createElement("span");
+    //     nameSpan.innerText = "Select Employee Name";
+    //     nameSpan.style.color = "red";
+    //     parentForm.append(nameSpan);
+    // } 
+    // if(!ExpenseNameField.checkValidity()) {
+    //     alert("Enter Expense Type");
+    // }
+    // if(!paymentTypeField.checkValidity()) {
+    //     alert("Select Payment Type");
+    // }
+    // if(!paymentMethodField.checkValidity()) {
+    //     alert("Select Payment Method");
+    // }
+
+    
+    var formValidity = document.getElementById("expenseForm")
+    if(formValidity.checkValidity()) {
         postJSONData();
-    }else{
-        alert("Fill the required field");
+    }else {
+        alert("Fill required fields");
     }
 
 }
-function expenseName(){
-    var nameReg = /[A-z 0-9]{4,15}/;
+
+function expenseName() {
+    var nameReg = /^[a-zA-Z 0-9\.@#&]{4,15}$/;
     var expenseNameInput = document.getElementById("expenseType").value;
     var expenseSpan = document.getElementById("nameError");
-    if(nameReg.test(expenseNameInput)){
+    if(nameReg.test(expenseNameInput)) {
         expenseSpan.classList.add("name-error");
-    }else{
+    }else {
         expenseSpan.classList.remove("name-error");
         expenseSpan.style.color = "red";
     }
 }
-function notesLimit(){
-    var notesReg = /[A-z 0-9]{4,25}/;
+
+function notesLimit() {
+    var notesReg = /^[a-zA-Z 0-9\.@&#]{4,250}$/;
     var notesInput = document.getElementById("expenseNotes").value;
     var notesSpan = document.getElementById("notesError")
     if(notesReg.test(notesInput)){
             notesSpan.classList.add("notes-error");  
-        }else{
+    }   else {
             notesSpan.classList.remove("notes-error");
             notesSpan.style.color = "red"; 
              
-    }
+        }
 }
-function editAndSave(element){
-    if(element.value == "Save"){
-          formValidation();  
-    }else{
-        putMethod();
-    }
+
+function editAndSave(element) {
+    if(element.value == "Save") {
+          formValidation(); 
+           
+    }   else {
+            putMethod();
+        }
 
 }
-function addNewData(){
-    var url = new URL("http://localhost/expense/html/index.html");
+
+function addNewData() {
+
+    var url = new URL(indexPage);
     window.location.href = url;
 }
-function cancelEdit(){
-    var url = new URL("http://localhost/expense/html/expensepage.html");
+
+function cancelEdit() {
+    var url = new URL("http://localhost/expense/Assessment/html/expensepage.html");
     window.location.href = url;
 }
+
